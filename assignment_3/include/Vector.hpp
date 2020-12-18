fab6608c76b5d24dbec42728d220af308785ca20
Emil <kaliber181@gmail.com>
2020-12-18 17:34:46 +0000

    .

--- assignment_3/include/Vector.hpp
+++ assignment_3/include/Vector.hpp
@@ -1,7 +1,83 @@
-#pragma once
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
 
 template <typename T>
 class Vector {
+public:
+  Vector();
+  ~Vector();
+  Vector(const Vector&);
+  Vector(Vector&&);
 
+  Vector& operator=(const Vector&);
+  Vector& operator=(Vector&&);
+
+  T& operator[](int);
+  int capacity() const;
+  bool empty() const;
+  int size() const;
+  void push_back(const T& element);
+  T& at(int index);
+
+
+
+private:
+  void check_rezise();
+
+  int m_capacity = 20;
+  T* m_elements = new T[m_capacity];
+
+  T* temp_ptr = nullptr;
+
+  int m_size = 0;
 };
-#include "Vector.cpp"
+
+template <class T>
+Vector<T>::Vector() {}
+
+template <class T>
+Vector<T>::~Vector() {}
+
+template <class T>
+int Vector<T>::capacity() const {
+  return m_capacity;
+}
+
+template <class T>
+bool Vector<T>::empty() const {
+  return m_size==0?true:false;
+}
+
+template <class T>
+int Vector<T>::size() const {
+  return m_size;
+}
+
+template <class T>
+void Vector<T>::push_back(const T& element) {
+  m_elements[m_size] = element;
+  m_size++;
+  this->check_rezise();;
+}
+
+template <class T>
+T& Vector<T>::at(int index) {
+  return m_elements[index];
+}
+
+template <class T>
+void Vector<T>::check_rezise() {
+  if(m_size == m_capacity) {
+    m_capacity = m_capacity + 20;
+    temp_ptr = m_elements;
+
+    m_elements = new T[m_capacity];
+    for (int i = 0; i < m_size; i++) {
+      m_elements[i] = temp_ptr[i];
+    }
+    delete[] temp_ptr;
+    temp_ptr = nullptr;
+  }
+}
+
+#endif
--- assignment_3/main.cpp
+++ assignment_3/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "Vector.hpp"
+#include <string>
 
-int main()
-{
-    std::cout << "Create a wishlist with your new vector! :)" << std::endl;
-    return 0;
+
+int main() {
+  Vector<int> v1;
+  int n = 3;
+  for (int i = 0; i < 100; i++) {
+    v1.push_back(n);
+    std::cout << v1.at(i) << " ";
+  }
+  std::cout << std::endl;
+  std::cout << v1.size() << std::endl;
+
+  Vector<std::string> v2;
+  if (v2.empty()) {
+    std::cout << "1 is empty" << std::endl;
+  }
+  for (int i = 0; i < 100; i++) {
+    v2.push_back("ya yeet");
+    std::cout << v2.at(i) << " ";
+  }
+  std::cout << v2.size() << std::endl;
+  std::cout << std::endl;
+
+  if (v2.empty()) {
+    std::cout << "2 is empty" << std::endl;
+  }
+
 }
